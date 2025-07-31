3f7905ef991ef725a3da5fc419c02e6b72dfc5ee
Eyasu <eyasugetaneh0@gmail.com>
2025-07-31 17:26:11 +0300

    refactor(main): initialize user repository, use case, controller, and router setup

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	controllers "blog-api/Delivery/Controllers"
+	router "blog-api/Delivery/Router"
 	infrastructure "blog-api/Infrastructure"
+	repositories "blog-api/Repositories"
+	usecases "blog-api/Usecases"
 )
 
 func main() {
 	infrastructure.InitMongo()
-	
+	userRepo := repositories.NewUserRepo(infrastructure.UserCollection())
+
+    userUsecase := usecases.NewUserUsecase(userRepo)
+
+    userController := controllers.NewUserController(userUsecase)
+
+    router := router.SetUpRouter(userController)
+
+    router.Run(":8080")
 }
