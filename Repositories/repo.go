3cc7fc8e32c4d34b1524ace51c5de3d3852b5630
Eyasu <eyasugetaneh0@gmail.com>
2025-08-01 10:53:44 +0300

    feat: implement validation methods

--- Repositories/repo.go
+++ Repositories/repo.go
@@ -4,7 +4,10 @@ import (
 	"blog-api/Domain"
 	"blog-api/Usecases"
 	"context"
+	"errors"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 type UserRepo struct {
@@ -21,4 +24,37 @@ func  NewUserRepo(c *mongo.Collection) usecases.UserRepoI {
 func (ur *UserRepo) Register(u *domain.User) error{ 
 	_, err := ur.Collection.InsertOne(ur.Context, u)
 	return err
+}
+
+func (ur *UserRepo) CheckUserExists(UsernameOrEmail,password string) (*domain.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var user domain.User
+
+	filter := bson.M{
+
+		"$or" : []bson.M{
+			{"username": UsernameOrEmail},
+			{"email": UsernameOrEmail},
+		},
+	}
+
+	err := ur.Collection.FindOne(ctx, filter).Decode(&user)
+	if err != nil{
+		return nil, errors.New("invalid credentials")
+	}
+
+	return &user, nil
+
+}
+
+func (ur *UserRepo) FindByEmail(email string) (*domain.User, error) {
+	var user domain.User
+
+	err := ur.Collection.FindOne(context.TODO(), bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
