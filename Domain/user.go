a5ac759e677268949512d94bc2434795fe9720f5
anansi-1 <anansisime7@gmail.com>
2025-08-03 22:37:55 +0300

    add UserRepository and UserUsecase interface

--- Domain/user.go
+++ Domain/user.go
@@ -1,26 +1,40 @@
 package domain
 
 import (
-    "go.mongodb.org/mongo-driver/bson/primitive"
-    "time"
+	"context"
+	"time"
 )
 
 type Role string
 
 const (
-    RoleUser  Role = "user"
-    RoleAdmin Role = "admin"
+	RoleUser  Role = "user"
+	RoleAdmin Role = "admin"
 )
 
 type User struct {
-    ID           primitive.ObjectID
-    Username     string
-    Email        string
-    Password     string
-    Role         Role
-    CreatedAt    time.Time
-    UpdatedAt    time.Time
-    Bio          string
-    ProfilePicture string
-	ContactInfo  string
+	ID             string
+	Password       string
+	Email          string
+	Username       string
+	Role           Role
+	IsVerified     bool
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	Bio            string
+	ProfilePicture string
+	ContactInfo    string
+}
+
+type IUserRepository interface {
+	Create(ctx context.Context, user *User) (*User, error)
+	GetByEmail(ctx context.Context, email string) (*User, error)
+	ExistsByEmail(ctx context.Context, email string) (bool, error)
+	GetByUsername(ctx context.Context, username string) (*User, error)
+	GetByID(ctx context.Context, id string) (*User, error)
+	ExistsByUsername(ctx context.Context, username string) (bool, error)
+}
+type IUserUsecase interface {
+	Register(ctx context.Context, user *User) error
+	Login(ctx context.Context, user *User) (*TokenResponse, error)
 }
