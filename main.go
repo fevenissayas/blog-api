20eb2063a309d5ff4fee2d45b31f6632ac2739a9
anansi-1 <anansisime7@gmail.com>
2025-08-04 00:59:26 +0300

    add NewServices

--- main.go
+++ main.go
@@ -1,23 +1,36 @@
 package main
 
 import (
+	"time"
+
 	controllers "blog-api/Delivery/Controllers"
 	router "blog-api/Delivery/Router"
 	infrastructure "blog-api/Infrastructure"
 	repositories "blog-api/Repositories"
 	usecases "blog-api/Usecases"
+
 )
 
 func main() {
+
 	infrastructure.LoadEnv()
-	infrastructure.InitMongo()
-	userRepo := repositories.NewUserRepo(infrastructure.UserCollection())
-    JwtService := infrastructure.NewJwtService()
-    userUsecase := usecases.NewUserUsecase(userRepo, JwtService)
 
-    userController := controllers.NewUserController(userUsecase)
+	client := repositories.NewMongoClient()
+	db := client.Database(infrastructure.Env.DB_NAME)
 
-	router := router.SetUpRouter(userController, jwtService)
-	
-    router.Run(":8080")
+	jwtService := infrastructure.NewJWTService()
+	passwordService := infrastructure.NewPasswordService()
+	userRepository := repositories.NewUserRepository(db)
+	refreshRepository := repositories.NewRefreshTokenRepository(db)
+	userUsecase := usecases.NewUserUseCase(userRepository,refreshRepository, jwtService, passwordService, 3*time.Second)
+	authUsecase :=usecases.NewAuthUsecase(jwtService,userRepository,refreshRepository,3*time.Second)
+	userController := controllers.NewUserController(userUsecase)
+	authController := controllers.NewAuthController(authUsecase)
+	r := router.SetupRouter(userController,authController)
+
+	port := infrastructure.Env.PORT
+	if port == "" {
+		port = "8080"
+	}
+	r.Run(":" + port)
 }
