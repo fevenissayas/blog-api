e87ea16f1a8a7e89c107cbddee989cde998c6c47
philapheda <abrhamyishak222@gmail.com>
2025-08-04 14:08:26 +0300

    added the controller, repository, domain, and usecases

---
+++ Repositories/blog_repository.go
@@ -0,0 +1,66 @@
+package repositories
+
+import (
+	domain "blog-api/Domain"
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type blogModel struct {
+	ID             primitive.ObjectID `bson:"_id"`
+	Title          string             `bson:"title"`
+	Content        string             `bson:"content"`
+	User           domain.User		  `bson:"user"`
+	Tags           string			  `bson:"tags"`
+	CreatedAt      time.Time          `bson:"createdAt"`
+	UpdatedAt      time.Time          `bson:"updatedAt"`
+}
+
+func toDomainBlog(m blogModel) domain.Blog {
+	return domain.Blog{
+		ID:             m.ID.Hex(),
+		Title:          m.Title,
+		Content:        m.Content,
+		Tags:			m.Tags,
+		User:			m.User,
+		CreatedAt:      m.CreatedAt,
+		UpdatedAt:      m.UpdatedAt,
+	}
+}
+
+type blogRepository struct {
+	blogCollection *mongo.Collection
+}
+
+func NewBlogRepository(db *mongo.Database) domain.IBlogRepository {
+
+	collection := db.Collection("blog")
+	return &blogRepository{blogCollection: collection}
+}
+
+func (r *blogRepository) Create(ctx context.Context, blog *domain.Blog) (*domain.Blog, error) {
+	blogObjectID := primitive.NewObjectID()
+	blog.ID = blogObjectID.Hex()
+
+	doc := bson.M{
+		"_id":             blogObjectID,
+		"title":           blog.Title,
+		"content":         blog.Content,
+		"tags":            blog.Tags,
+		"user":            blog.User,
+		"createdAt":       blog.CreatedAt,
+		"updatedAt":       blog.UpdatedAt,
+	}
+
+	_, err := r.blogCollection.InsertOne(ctx, doc)
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert user: %w", err)
+	}
+
+	return blog, nil
+}
